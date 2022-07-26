b0d0d389304dace9a35cccc7806ad3eb14ebd656
Willian Brusch <wbrusch@pop-os.localdomain>
2022-07-26 02:53:31 -0300

    fix: deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { TodoModule } from './todo/todo.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: 5433,
+      port: 5432,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
