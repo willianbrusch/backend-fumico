be044835d6689f3db30b5780cc947481f77686db
Willian Brusch <wbrusch@pop-os.localdomain>
2022-07-26 00:33:16 -0300

    fix: config PORT

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
